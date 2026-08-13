a9508e938b5c1e6d40c0f86b55bc17c447a15bdd
Benjamin Bannier <bbannier@gmail.com>
2026-08-13 09:39:04 +0200

    Migrating linting from prettier to biome

--- spicy-lint.js
+++ spicy-lint.js
@@ -8,9 +8,9 @@ const config = path.join(prefix, "sgconfig.yml");
 const sg = path.join(prefix, "node_modules", ".bin", "ast-grep");
 
 try {
-  execFileSync(sg, ["scan", "-c", config, ...process.argv.slice(2)], {
-    stdio: "inherit",
-  });
+	execFileSync(sg, ["scan", "-c", config, ...process.argv.slice(2)], {
+		stdio: "inherit",
+	});
 } catch (e) {
-  process.exit(e.status || 1);
+	process.exit(e.status || 1);
 }
