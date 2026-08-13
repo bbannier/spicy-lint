9f1ca667af1fd2b74fd46b03d589912c3757c7c6
Benjamin Bannier <bbannier@gmail.com>
2026-08-13 10:09:56 +0200

    Import Node dependency with `node:`

--- spicy-lint.js
+++ spicy-lint.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-const { execFileSync } = require("child_process");
-const path = require("path");
+const { execFileSync } = require("node:child_process");
+const path = require("node:path");
 
 const prefix = __dirname;
 const config = path.join(prefix, "sgconfig.yml");
